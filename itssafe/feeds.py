c4c035d6d2f0a1ac96926aac987ed61fcc94c155
Navid.Goalpure <navid.goalpure@gmail.com>
2017-02-07 19:06:14 +0330

    bug fixed

--- itssafe/feeds.py
+++ itssafe/feeds.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib.syndication.views import Feed
 from itssafe.models import itssafePosts
-from itsnormal.models import itsNormalPosts
-from itertools import chain
 
 class LatestItssafeFeed(Feed):
     title = "نجوای جدید سلامت در دوران بارداری"
@@ -10,7 +8,7 @@ class LatestItssafeFeed(Feed):
     description = "آپدیت و اضافه کردن مطالب جدید در ارتباط با سلامت مادران باردار"
 
     def items(self):
-        return chain(itssafePosts.objects.order_by('publication_date')[:5] , itsNormalPosts.objects.order_by('publication_date')[:5])
+        return itssafePosts.objects.order_by('publication_date')[:5]  
 
     def item_title(self, item):
         return item.titr
